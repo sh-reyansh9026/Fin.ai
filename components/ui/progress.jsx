372eed2ef5312ecd693b0d83ae66e41f9495193b
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2025-05-22 01:37:29 +0530

    Build Dashboard page alongwith the functionality to update budget

--- components/ui/progress.jsx
+++ components/ui/progress.jsx
@@ -1,29 +1,27 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Progress({
-  className,
-  value,
-  ...props
-}) {
-  return (
-    (<ProgressPrimitive.Root
-      data-slot="progress"
+const Progress = React.forwardRef(
+  ({ className, value, extraStyles, ...props }, ref) => (
+    <ProgressPrimitive.Root
+      ref={ref}
       className={cn(
-        "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
+        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
         className
       )}
-      {...props}>
+      {...props}
+    >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
-    </ProgressPrimitive.Root>)
-  );
-}
+        className={`bg-primary h-full w-full flex-1 transition-all ${extraStyles}`}
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  )
+);
 
-export { Progress }
+export { Progress };
