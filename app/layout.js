d5933cae54bf5120baab68dca717f3af712cceae
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2025-05-24 01:21:10 +0530

    Error resolved

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -7,31 +8,25 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ai-finance",
+  title: "Fin.AI",
   description: "A finance platform powered by AI",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
     <html lang="en">
-      <body className={`${inter.className}`}>
-        {/* Header */}
-        <Header />
-        <main className="min-h-screen">
-          {children}
-          </main>
-          <Toaster richColors/>
-        {/* Footer */}
-        <footer className = "bg-blue-50 py-12">
-          <div className = "container mx-auto px-4 text-center text-gray-600">
-            <p>
-              © 2025 AI Finance. All rights reserved.
-            </p>
-          </div>
-        </footer>
+      <body className={inter.className}>
+        <ClerkProvider>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>© 2025 AI Finance. All rights reserved.</p>
+            </div>
+          </footer>
+        </ClerkProvider>
       </body>
-      </html>
-      </ClerkProvider>
+    </html>
   );
 }
