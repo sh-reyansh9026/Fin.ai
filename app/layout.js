a3f7475e55f60879be8330c7128b6d191d9969b4
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2025-05-31 00:19:48 +0530

    modified globals.css

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+// app/layout.js (or layout.tsx if using TypeScript)
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -14,15 +16,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <head>
-          <link rel="icon" href="/logo-sm.png" sizes="any" />
-        </head>
-        <body className={`${inter.className}`}>
+      <html lang="en" className={inter.className}>
+        <body>
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
-
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with 💗 by RoadsideCoder</p>
